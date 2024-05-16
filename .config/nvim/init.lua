4165e82574a5241c2e07563a6ddb44e923c582ff
Quinn Collins <collinsqui@gmail.com>
2024-05-15 22:07:18 -0400

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -428,7 +428,14 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
+        emmet_language_server = {},
+        emmet_ls = {},
         gopls = {},
+        html = {},
+        htmlbeautifier = {},
+        htmlhint = {},
+        htmx = {},
+
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
