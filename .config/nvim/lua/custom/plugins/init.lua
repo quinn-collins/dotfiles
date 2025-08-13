b282cb9a499f25a5ebd6a7f3d5460151f137c390
Quinn Collins <collinsqui@gmail.com>
2025-08-12 20:37:32 -0400

    Updates

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,41 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'simonmclean/triptych.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'nvim-tree/nvim-web-devicons', -- optional for icons
+      'antosha417/nvim-lsp-file-operations', -- optional LSP integration
+    },
+    opts = {
+      extension_mappings = {
+        ['<c-f>'] = {
+          mode = 'n',
+          fn = function(target, _)
+            require('telescope.builtin').live_grep {
+              search_dirs = { target.path },
+            }
+          end,
+        },
+      },
+    }, -- config options here
+    keys = {
+      { '<leader>nt', ':Triptych<CR>' },
+    },
+  },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    build = ':Copilot auth',
+    event = 'InsertEnter',
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+  },
+  {
+    'giuxtaposition/blink-cmp-copilot',
+  },
+}
